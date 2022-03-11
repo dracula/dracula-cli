c443d61e12aa2e4771dcc44569c2f7fe661247d8
Wasi Master <arianmollik323@gmail.com>
2022-03-11 22:15:53 +0600

    Bump version to 0.1.1

--- dracula/__init__.py
+++ dracula/__init__.py
@@ -1,6 +1,6 @@
 """A CLI for draculatheme.org"""
 __name__ = "pypi-command-line"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Wasi Master"
 __license__ = "MIT"
 __github__ = "https://github.com/dracula/dracula-cli"
